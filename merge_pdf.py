c36a6d5f3da871e03c1aa831fd75c8024389b8c6
sherzog85 <98664912+sherzog85@users.noreply.github.com>
2022-01-29 21:34:37 +0100

    Update merge_pdf.py

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,13 +1,14 @@
-
-
 import PyPDF2
-pdfReader = PyPDF2.PdfFileReader(open('dogs_3.pdf', 'rb'))
+
+#load documents to merge
+pdfReader = PyPDF2.PdfFileReader(open('dogs.pdf', 'rb'))
 pdfOverlayReader = PyPDF2.PdfFileReader(open('overlay.pdf', 'rb'))
+#open output stream
 pdfWriter = PyPDF2.PdfFileWriter()
 for i in range(2):
-    minutesFirstPage = pdfReader.getPage(i)
-    minutesFirstPage.mergePage(pdfOverlayReader.getPage(0))
-    pdfWriter.addPage(minutesFirstPage)
+    OIDPage = pdfReader.getPage(i)
+    OIDPage.mergePage(pdfOverlayReader.getPage(0))
+    pdfWriter.addPage(OIDPage)
 resultPdfFile = open('dogs_box.pdf', 'wb')
 pdfWriter.write(resultPdfFile)
 resultPdfFile.close()
